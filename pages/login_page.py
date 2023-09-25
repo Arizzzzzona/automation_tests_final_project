40c5f12ef2a64e920bf3f7751027b7ea2debcd56
Sasha <arizzzzzona@yandex.ru>
2023-09-25 17:04:31 +0300

    add new tests for login_page

---
+++ pages/login_page.py
@@ -0,0 +1,28 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+import time
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        #проверка на корректный url адрес
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+        time.sleep(5)
+        assert "login" in self.browser.current_url,"It's not Login link"
+
+    def should_be_login_form(self):
+        # проверка, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        
+
+    def should_be_register_form(self):
+        #проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
+        
